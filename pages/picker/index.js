7623674491fdd664116ab7e188e813b7210315aa
Allison12345 <2562392373@qq.com>
2020-02-19 21:42:08 +0800

    picker page

---
+++ pages/picker/index.js
@@ -0,0 +1,89 @@
+Page({
+  data: {
+    singleArray: ['美国', '中国', '巴西', '日本'],
+    objectArray: [
+      {
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 3,
+    multiArray: [
+      ['无脊柱动物', '脊柱动物'],
+      ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'],
+      ['猪肉绦虫', '吸血虫']
+    ],
+    objectMultiArray: [
+      [
+        {
+          id: 0,
+          name: '无脊柱动物'
+        },
+        {
+          id: 1,
+          name: '脊柱动物'
+        }
+      ],
+      [
+        {
+          id: 0,
+          name: '扁性动物'
+        },
+        {
+          id: 1,
+          name: '线形动物'
+        },
+        {
+          id: 2,
+          name: '环节动物'
+        },
+        {
+          id: 3,
+          name: '软体动物'
+        },
+        {
+          id: 4,
+          name: '节肢动物'
+        }
+      ],
+      [
+        {
+          id: 0,
+          name: '猪肉绦虫'
+        },
+        {
+          id: 1,
+          name: '吸血虫'
+        }
+      ]
+    ],
+    multiIndex: [1, 2, 1],
+    date: '2016-09-01',
+    time: '12:01',
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部'
+  },
+  onPickerChange(e) {
+    console.log(e.detail.value)
+    this.setData({ index: e.detail.value })
+  },
+  bindMultiPickerChange(e) {
+    console.log(e, '1')
+    this.setData({ index: e.detail.value })
+  },
+  bindMultiPickerColumnChange(e) {
+    console.log(e, '2')
+  }
+})
